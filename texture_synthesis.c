0d9ee25aadd3dff3b241e110f620f39fae219591
acraviogli <acraviogli@eth.ch>
2025-04-30 15:36:19 +0200

    solve synthesis and generate gauss pyramid

--- texture_synthesis.c
+++ texture_synthesis.c
@@ -3,19 +3,24 @@
 #include "gauss_filter.h"
 #include "image_struct.h"
 #include "find_best_match.h"
+#include "gauss_pyramid.h"
 
-int *texture_synthesis(int *Ia, int output_size, int pyramid_levels) {
-    // initialize image with random noise
+int *texture_synthesis(struct Image Ia, int output_size, int pyramid_levels) {
     srand(0);
     int arraySize = 3 * output_size * output_size;
-    int *Is = (int*)malloc(arraySize * sizeof(int));
-    int i;
-    for (i = 0; i < arraySize; i++) {
-        Is[i] = rand() % 255;
+    // initialize image with random noise
+    struct Image Is;
+    Is.x = output_size;
+    Is.y = output_size;
+    Is.rgb_clamp = Ia.rgb_clamp;
+    Is.pixels = (int*)malloc(arraySize * sizeof(int));
+
+    for (int i = 0; i < arraySize; i++) {
+        Is.pixels[i] = rand() % 255;
     }
     // create the gaussian pyramids
-    struct Image *Gs; // Is
-    struct Image *Ga; // Ia
+    struct Image *Gs = build_gauss_pyramid(Is); // Is
+    struct Image *Ga = build_gauss_pyramid(Ia); // Ia
 
     for (int l = pyramid_levels - 1; l >= 0; l--) {
         for (int i = 0; i < Gs[l].x; i++) {
