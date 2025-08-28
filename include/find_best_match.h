61fd24624d76cfe488d965bc3d6a741082fb5092
Arseni Loika <aloika@student.ethz.ch>
2025-08-28 14:15:45 +0200

    what

--- include/find_best_match.h
+++
@@ -1,4 +0,0 @@
-#include "image.h"
-
-int find_best_match(Image *Ga, Image *Gs, int L, int total_levels, int xs,
-                    int ys);
--- include/texture_synthesis.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYNTHESIZER_H
-#define SYNTHESIZER_H
-
-#include "image.h"
-
-Image texture_synthesis(Image Ia, int output_size, int pyramid_levels);
-
-#endif
